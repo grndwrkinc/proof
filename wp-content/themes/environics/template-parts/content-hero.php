b40dea0496aa42a27ed9fa8eb7a4c848e117fb90
Lynn Gammie <lynn@grndwrk.ca>
2018-02-02 17:36:27 -0500

    Erasing OG site sass from posts-and-pages (not finished)

--- wp-content/themes/environics/template-parts/content-hero.php
+++ wp-content/themes/environics/template-parts/content-hero.php
@@ -17,26 +17,6 @@
 		<!-- svg content goes here -->
 		<img src="/wp-content/themes/environics/assets/images/Environics_Communications_logo_white.svg" alt="">
 	</div>
-
-	<!-- Mobile version -->
-	<div class="hero hero-mobile" style="background-image: url( <?php echo $hero; ?> )">
-		<div class="text">
-			<div class="container">
-				<svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-					 width="296px" height="180px" viewBox="0 0 296 355" enable-background="new 0 0 296 355" xml:space="preserve">
-				<!-- <title>E</title>
-				<desc>Created with Sketch.</desc> -->
-				<g id="Page-2">
-					<g id="Mobile" transform="translate(-265.000000, -448.000000)">
-						<path id="mobile-E" fill="#FFFFFF" d="M343.5,517H561v-69H265v49.5v257V803h296v-68H343.5v-76H561v-67H343.5V517z"/>
-					</g>
-				</g>
-				</svg>
-				<h1><?php the_title(); ?></h1>
-				<p><?php the_content(); ?></p>
-			</div>
-		</div>
-	</div>
 <?php 
 
 	//Default Hero (No video)
