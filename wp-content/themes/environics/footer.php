f8ca53f120e91cb49f0fb6a5cee15d78b1096853
Lynn Gammie <lynn@grndwrk.ca>
2018-02-28 12:39:20 -0500

    Conditionals for header/footer social links

--- wp-content/themes/environics/footer.php
+++ wp-content/themes/environics/footer.php
@@ -17,11 +17,21 @@
 			<div class="footer-links"><?php wp_nav_menu( array( 'menu' => 'Footer Menu', ) ); ?></div>
 			<p class="copyright">©<?php echo date('Y');?> Proof</p>
 			<div class="social-icons">
-				<a href="<?php the_field('twitter_account', 'option'); ?>" target="_blank" alt="Twitter link" aria-label="Link to Environics Twitter account"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-				<a href="<?php the_field('facebook_account', 'option'); ?>" target="_blank" alt="Facebook link" aria-label="Link to Environics Facebook account"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-				<a href="<?php the_field('instagram_account', 'option'); ?>" target="_blank" alt="Instagram link" aria-label="Link to Environics Instagram account"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-				<a href="<?php the_field('youtube_account', 'option'); ?>" target="_blank" alt="Youtube link" aria-label="Link to Environics Youtube account"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-				<a href="<?php the_field('linkedin_account', 'option'); ?>" target="_blank" alt="LinkedIn link" aria-label="Link to Environics LinkedIn account"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<?php if(get_field('twitter_account', 'option')): ?>
+					<a href="<?php the_field('twitter_account', 'option'); ?>" target="_blank" alt="Twitter link" aria-label="Link to Environics Twitter account"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('facebook_account', 'option')): ?>
+					<a href="<?php the_field('facebook_account', 'option'); ?>" target="_blank" alt="Facebook link" aria-label="Link to Environics Facebook account"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('instagram_account', 'option')): ?>
+					<a href="<?php the_field('instagram_account', 'option'); ?>" target="_blank" alt="Instagram link" aria-label="Link to Environics Instagram account"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('youtube_account', 'option')): ?>
+					<a href="<?php the_field('youtube_account', 'option'); ?>" target="_blank" alt="Youtube link" aria-label="Link to Environics Youtube account"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('linkedin_account', 'option')): ?>
+					<a href="<?php the_field('linkedin_account', 'option'); ?>" target="_blank" alt="LinkedIn link" aria-label="Link to Environics LinkedIn account"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<?php endif; ?>
 			</div>
 		</div>
 	</footer>
--- wp-content/themes/environics/header.php
+++ wp-content/themes/environics/header.php
@@ -52,11 +52,21 @@
 			<div class="menu-primary-container outer">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu' => 'Primary' ) ); ?>
 				<div class="social-icons">
+					<?php if(get_field('twitter_account', 'option')): ?>
 					<a href="<?php the_field('twitter_account', 'option'); ?>" target="_blank" alt="Twitter link" aria-label="Link to Environics Twitter account"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('facebook_account', 'option')): ?>
 					<a href="<?php the_field('facebook_account', 'option'); ?>" target="_blank" alt="Facebook link" aria-label="Link to Environics Facebook account"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('instagram_account', 'option')): ?>
 					<a href="<?php the_field('instagram_account', 'option'); ?>" target="_blank" alt="Instagram link" aria-label="Link to Environics Instagram account"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('youtube_account', 'option')): ?>
 					<a href="<?php the_field('youtube_account', 'option'); ?>" target="_blank" alt="Youtube link" aria-label="Link to Environics Youtube account"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+				<?php endif; ?>
+				<?php if(get_field('linkedin_account', 'option')): ?>
 					<a href="<?php the_field('linkedin_account', 'option'); ?>" target="_blank" alt="LinkedIn link" aria-label="Link to Environics LinkedIn account"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<?php endif; ?>
 				</div>
 			</div>
 		</nav>
