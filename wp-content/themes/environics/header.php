51f109032702ee47e02b6f8aa0570f8f4532e990
almasir <richard@grndwrk.ca>
2018-03-05 11:00:28 -0500

    Add Google Tag Manager

--- wp-content/themes/environics/header.php
+++ wp-content/themes/environics/header.php
@@ -20,10 +20,23 @@
 <link rel="icon" href="/wp-content/themes/environics/assets/images/Proof_Social_Avatar_YouTube.jpg" sizes="192x192">
 <link rel="apple-touch-icon-precomposed" href="/wp-content/themes/environics/assets/images/Proof_Social_Avatar_YouTube.jpg">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TVZ74DS');</script>
+<!-- End Google Tag Manager -->
+
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVZ74DS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'environics' ); ?></a>
 	<!-- If the user is new to the site and welcome msg is turned on in options, display a welcome msg -->
